1ee2e38d6a00d79c6496b1d920f0d48ee019da42
Tu Nombre <jua.fuentese@duocuc.cl>
2025-08-20 18:43:54 -0400

    fix tailwind.config.ts x error de ejecucion

--- mineria-del-sur/tailwind.config.js
+++ mineria-del-sur/tailwind.config.js
@@ -1,9 +1,11 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",   // si usas carpeta components fuera de src
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
-
